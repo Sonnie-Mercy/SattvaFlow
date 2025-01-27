def9480d3ad68580c42c7db9bb2aebbce78c8db7
Mercy Muthoni Ndirangu <105234288+Sonnie-Mercy@users.noreply.github.com>
2025-01-27 17:29:38 +0300

    Making sure the front page renders

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,76 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import axios from "axios";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-import { Provider } from "react-redux";
-import { store } from "./app/store.js";
-import { getUserFromStorage } from "./features/user/userSclice.js";
-
-import PrivateRoute from "./components/PrivateRoute.jsx";
-import Home from "./pages/Home.jsx";
-import SignUp from "./pages/SignUp.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import YogaPage from "./pages/YogaPage.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
 import Landing from "./pages/Landing.jsx";
+import "./index.css"; // Importing the CSS file
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
   },
-  {
-    path: "login",
-    element: <Login />,
-  },
-  {
-    path: "signup",
-    element: <SignUp />,
-  },
-  {
-    path: "",
-    element: <PrivateRoute />,
-    children: [
-      {
-        path: "",
-        element: <App />,
-        children: [
-          {
-            path: "home",
-            element: <Home />,
-          },
-          {
-            path: "register",
-            element: <Register />,
-          },
-          {
-            path: "yoga-class/:id",
-            element: <YogaPage />,
-          },
-          {
-            path: "dashboard",
-            element: <Dashboard />,
-          },
-        ],
-      },
-    ],
-  },
+  // other routes can be added here
 ]);
-
-store.dispatch(getUserFromStorage());
-
-// axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.baseURL = "https://sattvaflow.onrender.com"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      {/* <App /> */}
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- frontend/src/pages/YogaPage.jsx
+++ frontend/src/pages/YogaPage.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const YogaPage = () => {
   return (
     <>
-      <div className="flex flex-col h-screen justify-center items-center gap-5">
-        <p className="text-4xl font-black text-gray-900 ">FLow with Sattva Flow</p>
-        <p className="text-4xl font-black text-gray-900 ">🧘‍♂️</p>
+      <div className="flex flex-col h-screen justify-center items-center gap-5 bg-purple-200">
+        <p className="text-4xl font-black text-purple-900">Flow with Sattva Flow</p>
+        <p className="text-4xl font-black text-purple-900">🧘‍♂️</p>
         <Link
           to="/home"
-          className="text-white bg-violet-500 hover:bg-violet-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+          className="text-white bg-violet-600 hover:bg-violet-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
         >
           Thank you for choosing yoga with Us
         </Link>
